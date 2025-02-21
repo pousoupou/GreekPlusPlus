c01ad1d0abc84c61f3d317f786a82b051f0af571
Stilianos Papaspiros <stelios.papaspiros@gmail.com>
2025-02-21 19:18:40 +0200

    Lexer function complete

--- greekpp_4990_5162.py
+++ greekpp_4990_5162.py
@@ -1,30 +1,20 @@
-# -*- coding: utf-8 -*-
-
 import sys
 import string
 
 ### ============= Helper Functions =============
-def open_gpp_source_file(file_name):
-    return open(file_name, 'r')
-
-def close_gpp_source_file(file_name):
-    file_name.close()
-
 def categorize(char):
-    if char.isalpha():
+    if char.isalpha() or char == '_':
         return "alpha"
     elif char.isnumeric():
         return "digit"
     elif char == '+' or char == '-' or char == '*' or char == '/':
         return "arithmOp"
-    elif char == '<':
-        return "smaller"
-    elif char == '>':
-        return "bigger"
+    elif char == '<' or char == '>':
+        return "relOp"
     elif char == ':':
         return "asgn"
     elif char == "=":
-        return "relationOp"
+        return "equal"
     elif char == ';' or char == ',':
         return "delimiter"
     elif char == '(' or char == ')' or char == '[' or char == ']':
@@ -33,23 +23,21 @@ def categorize(char):
         return "comment"
     elif char == '%':
         return "pointer"
+    elif char == ' ' or char == '\t':
+        return "whitespace"
     elif char == '':
         return "EOF"
     else:
         return "character error"
     
-
-
 ### ============================================
 
-### ============= Lexer =============
-
 ## ==== Global Definitions ====
-line_number = 1
+line_number = 0
 
 state = "start"
 
-keywords = (
+keywords = [
     "πρόγραμμα",    "δήλωση",       "εάν",
     "τότε",         "αλλιώς",       "εάν_τέλος",
     "επανέλαβε",    "μέχρι",        "όσο",
@@ -62,7 +50,7 @@ keywords = (
     "αρχή_συνάρτησης",              "τέλος_συνάρτησης",
     "αρχή_διαδικασίας",             "τέλος_διαδικασίας",
     "αρχή_προγράμματος",            "τέλος_προγράμματος"
-)
+]
 
 """ Token class:
 
@@ -80,60 +68,210 @@ class Token:
         self.line_number = line_number
 
     def __str__(self):
-        return self.recognized_string + "\t family: " + self.family + "\t line: " + str(self.line_number)
+        return self.recognized_string + "\t family: " + self.family + "\t line: " + str(self.line_number + 1)
 
 ## ============================
 
-def lexer():
-    source_file = sys.argv[1]
-    fd = open_gpp_source_file(source_file)
+### ============= Lexer =============
+class Lexer:
+    program_lines = []
+    line_index = 0
 
-    token = Token()
+    def __init__(self, file_path):
+        global line_number
+        self.file_path = file_path
 
-    char = ''
-    while not (char == '\n' or char == ' ' or char == '\t'):
-        char = fd.read(1)
-        token.recognized_string += char
+        if not(file_path.endswith(".gpp")):
+            print("Filetype Error: Not a .gpp file")
+            exit()
+        
+        fd = open(file_path, "r", encoding="utf-8")
+        self.program_lines = [line.rstrip() for line in fd]
+        # DEBUG
+        # print(self.program_lines)
+        fd.close()
 
-        category = categorize(char)
+    def __error(self, case):
+        print("Lexer error in line: " + str(line_number))
 
-        if (state == "start" and category == "alpha"):
-            state = "idk"
-        elif (state == "start" and category == "digit"):
-            state = "dig"
-        elif (state == "start" and category == "arithmOp"):
-            state == "arithmetic Operator"
-        elif (state == "start" and category == "asgn"):
-            state == "assignment"
-        elif (state == "start" and category == "delimiter"):
-            state == "delimiter"
-        elif (state == "start" and category == "group"):
-            state = "group"
-        elif (state == "start" and category == "bigger"):
-            state = "bigger"
-        elif (state == "start" and category == "smaller"):
-            state = "smaller"
-        elif (state == "start" and category == "relationOp"):
-            state = "relation Operator"
-        elif (state == "start" and category == "comment"):
-            state = "comment"
-        elif (state == "start" and category == "pointer"):
-            state = "pointer"
-        elif (state == "start" and category == "EOF"):
-            state = "EOF"
-        elif (state == "start" and category == "character error"):
-            state = "character error"
-        elif (state == "idk" and (category == "alpha" or category == "digit")):
-            state = "idk"
-        elif state == "idk" and 
-        else:
-            state = "unknown error"
+        if case == "number":
+            print("\tIllegal number constant. Can not contain non-numerical values")
+        elif case == "limits":
+            print("\tIllegal length of variable name")
+        elif case == "EOF":
+            print("\tEOF reached. Comments are opened but never closed")
+        elif case == "unknown":
+            print("\tUnknown character error")
+        elif case == "assign":
+            print("\t'=' expected")
+        
+        exit()
+        
+    def nextToken(self):
+        global line_number
+        current_line = ""
+        current_string = ""
+        family = ""
 
-    if char == '\n':
-        line_number += 1
+        var_name_len = 30
 
+        line_length = len(self.program_lines[line_number])
+        while (line_length == 0 or self.line_index >= line_length):
+            line_number += 1
+            self.line_index = 0
+            
+            if line_number >= len(self.program_lines):
+                exit()
 
+            line_length = len(self.program_lines[line_number])
 
-    close_gpp_source_file(source_file)
+        current_line = [line.rstrip() for line in self.program_lines[line_number]]
 
+        while(True):
+            char = current_line[self.line_index]
+            category = categorize(char)
+
+            if char == '':
+                self.line_index += 1
+            
+            elif category == "alpha":
+                while category == "alpha" or category == "digit":
+                    current_string += char
+
+                    var_name_len -= 1
+                    self.line_index += 1
+
+                    if var_name_len < 0:
+                        self.__error("limits")
+                    if self.line_index >= len(current_line):
+                        break
+
+                    char = current_line[self.line_index]
+                    category = categorize(char)
+
+                if current_string in keywords:
+                    family = "keyword"
+                    break
+                else:
+                    family = "id"
+                    break
+
+            elif category == "digit":
+                while category == "digit":
+                    current_string += char
+                    self.line_index += 1
+
+                    if category == "alpha":
+                        self.__error("number")
+                    if self.line_index >= len(current_line):
+                        break
+
+                    char = current_line[self.line_index]
+                    category = categorize(char)
+
+                family = "number"
+                break
+
+            elif category == "delimiter":
+                current_string += char
+                self.line_index += 1
+
+                family = "delimiter"
+                break
+
+            elif category == "arithmOp":
+                current_string += char
+                self.line_index += 1
+                
+
+                family = "arithmOperator"
+                break
+
+            elif category == "relOp":
+                current_string += char
+                self.line_index += 1
+                
+                char = current_line[self.line_index]
+                category = categorize(char)
+
+                if category == "equal":
+                    current_string += char
+                    
+                family = "relationalOperator"
+                break
+
+            elif category == "asgn":
+                current_string += char
+                self.line_index += 1
+
+                char = current_line[self.line_index]
+                category = categorize(char)
+
+                if category == "equal":
+                    current_string += char
+                    family = "assignOperator"
+                    break
+                else:
+                    self.__error("assign")
+            
+            elif category == "group":
+                current_string += char
+                self.line_index += 1
+
+                family = "group"
+                break
+
+            elif category == "comment":
+                if char == '{':
+                    while not char == "}":
+                        self.line_index += 1
+                        char = current_line[self.line_index]
+
+                        if char == '\n':
+                            line_number += 1
+                            self.line_index = 0
+
+                            char = current_line[self.line_index]
+                        elif char == '':
+                            self.__error("EOF")
+                            break
+                    
+                    break
+
+                else:
+                    self.__error("unknown")
+                    break
+
+            elif category == "pointer":
+                current_string += char
+                self.line_index += 1
+
+                family = "pointer"
+                break
+
+            elif category == "character error":
+                self.__error("unknown")
+            else:
+                self.__error("unknown")
+
+        next_token = Token(current_string, family, line_number)
+        print(next_token)
+        return next_token
+    
 ### =================================
+
+def main():
+    lexer = Lexer(sys.argv[1])
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+    lexer.nextToken()
+
+main()
