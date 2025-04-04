89307936a0f187f43f242d2aacc02fa784ff2143
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2025-04-04 15:22:19 +0300

    Update gpp.py

--- gpp.py
+++ gpp.py
@@ -953,20 +953,23 @@ class Parser:
         
 ### ==================================
 
+### ============= Quad =============
+class Quad:
+    def __init__(self, label, op, op1, op2, op3):
+        self.label = label
+        self.op = op
+        self.op1 = op1
+        self.op2 = op2
+        self.op3 = op3
+
+    def __str__(self):
+        return "{self.label}: {self.op}, {self.op1}, {self.op2}, {self.op3}"
+
+### ==================================
+
 def main():
     start = timer()
     lexer = Lexer(source_file)
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
-    # lexer.nextToken()
 
     parser = Parser(lexer)
     parser.syntax_analyzer()
